2f9470fc7d3a2e8d854e445aabf91e391c8ea2ab
aditya <adityayadav135952@gmail.com>
2024-09-27 15:19:08 +0530

    completed

--- src/contexts/TodoContext.js
+++ src/contexts/TodoContext.js
@@ -1,25 +1,3 @@
-// import { createContext, useContext } from "react";
-
-// export const TodoContext = createContext({
-//     Todos : [
-//         {
-//             id: 1, 
-//             todo: "Todo msg",
-//             comleted: false, 
-//         }
-//     ], 
-//     addTodo:(todo) => {}, 
-//     updateTodo: (id, todo) => {}, 
-//     deleteTodo: (id) => {}, 
-//     toggleComplete: (id) => {}
-// }) 
-
-// export const TodoInfo = () => {
-//     return useContext(TodoContext)
-// }
-
-// export const ToDoProvider = TodoContext.Provider
-
 import { useContext,createContext } from "react";
 
 
